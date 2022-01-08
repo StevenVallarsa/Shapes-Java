f3ef0b979652cf1bbb0ab269cbd739ae05846116
StevenVallarsa <orionsjv@gmail.com>
2022-01-07 21:19:26 -0500

    first commit

---
+++ src/main/java/com/sv/shapes2022/Shape.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sv.shapes2022;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-01-07
+ * purpose: First program using basic inheritance
+ */
+abstract class Shape {
+    
+    private String color;
+    abstract double GetArea();
+    abstract double GetPerimeter();
+    
+}
